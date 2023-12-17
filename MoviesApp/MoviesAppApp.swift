954de3806e5eea9e67d6c348f080ca4ed3aad8ff
Thomas Sheppard <thomas.sheppard@2innovateit.com>
2023-12-17 17:25:36 -0300

    Initialize project

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Thomas Sheppard on 17/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
