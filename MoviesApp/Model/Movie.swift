98e53bce2e47744f4af33af283a9300a584d3a5b
Thomas Sheppard <thomas.sheppard@2innovateit.com>
2023-12-20 12:38:45 -0300

    Realm para guardar localmente datos

--- MoviesApp/Model/Movie.swift
+++ MoviesApp/Model/Movie.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import RealmSwift
+import Realm
 
 struct MovieListResponseMessage : Decodable {
     let items : [Movie]
@@ -45,6 +47,48 @@ struct Movie : Codable, Hashable {
         return Movie(id: "tt1649418", title: "The Gray Man", fullTitle: "The Gray Man", year: "2022", releaseState: "22 Jul 2022", image: "https://m.media-amazon.com/images/M/MV5BOWY4MmFiY2QtMzE1YS00NTg1LWIwOTQtYTI4ZGUzNWIxNTVmXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_UX128_CR0,4,128,176_AL_.jpg", runtimeMins: "122", runtimeStr: "122 mins", plot: "When the CIA's most skilled operative-whose true identity is known to none-accidentally uncovers dark agency secrets, a psychopathic former colleague puts a bounty on his head, setting off a global manhunt by international assassins", contentRating: "PG-13", imDbRating: "6.6", imDbRatingCount: "65523", metacriticRating: "49", genres: "Action, Thriller", genreList: [KeyValue(key: "Action", value: "Action"), KeyValue(key: "Thriller", value: "Thriller")], directors: "Anthony Russo, Joe Russo", directorList: [IdName(id: "nm0751577", name: "Anthony Russo"), IdName(id: "nm0751648", name: "Joe Russo")], stars: "Ryan Gosling, Chris Evans, Ana de Armas, Billy Bob Thornton", starList: [IdName(id: "nm0331516", name: "Ryan Gosling"), IdName(id: "nm0262635", name: "Chris Evans")])
     }
     
+    func getPersistedObject() -> RealmMovie {
+        var realmMovie = RealmMovie()
+        realmMovie.id = self.id
+        realmMovie.title = self.title
+        realmMovie.fullTitle = self.fullTitle
+        realmMovie.year = self.year
+        realmMovie.releaseState = self.releaseState
+        realmMovie.image = self.image
+        realmMovie.runtimeMins = self.runtimeMins
+        realmMovie.runtimeStr = self.runtimeStr
+        realmMovie.plot = self.plot
+        realmMovie.contentRating = self.contentRating
+        realmMovie.imDbRating = self.imDbRating
+        realmMovie.imDbRatingCount = self.imDbRatingCount
+        realmMovie.metacriticRating = self.metacriticRating
+        realmMovie.genres = self.genres
+        realmMovie.genreList = List()
+        self.genreList.forEach({ item in
+            var keyValue = RealmKeyValue()
+            keyValue.key = item.key
+            keyValue.value = item.value
+            realmMovie.genreList.append(keyValue)
+        })
+        realmMovie.directorList = List()
+        self.directorList.forEach({ item in
+            var idName = RealmIdName()
+            idName.id = item.id
+            idName.name = item.name
+            realmMovie.directorList.append(idName)
+        })
+        realmMovie.directors = self.directors
+        realmMovie.stars = self.stars
+        realmMovie.starList = List()
+        self.starList.forEach({ item in
+            var idName = RealmIdName()
+            idName.id = item.id
+            idName.name = item.name
+            realmMovie.starList.append(idName)
+        })
+        return realmMovie
+    }
+    
 }
 
 struct KeyValue : Codable, Hashable {
@@ -55,4 +99,52 @@ struct KeyValue : Codable, Hashable {
 struct IdName : Codable, Hashable {
     let id : String
     let name : String
+}
+
+
+//MARK: - Realm Objects
+
+class RealmMovie : Object {
+    @Persisted var id : String
+    @Persisted var title : String
+    @Persisted var fullTitle : String
+    @Persisted var year : String
+    @Persisted var releaseState : String
+    @Persisted var image : String
+    @Persisted var runtimeMins : String
+    @Persisted var runtimeStr : String
+    @Persisted var plot : String
+    @Persisted var contentRating : String
+    @Persisted var imDbRating : String
+    @Persisted var imDbRatingCount : String
+    @Persisted var metacriticRating : String
+    @Persisted var genres : String
+    @Persisted var genreList : List<RealmKeyValue>
+    @Persisted var directors : String
+    @Persisted var directorList : List<RealmIdName>
+    @Persisted var stars : String
+    @Persisted var starList : List<RealmIdName>
+    
+    func getMovie() -> Movie {
+        let genreList = Array(self.genreList.map({ realmKeyVale in
+            return KeyValue(key: realmKeyVale.key, value: realmKeyVale.value)
+        }))
+        let directorList = Array(self.directorList.map({ realmIdName in
+            return IdName(id: realmIdName.id, name: realmIdName.name)
+        }))
+        let starList = Array(self.starList.map({ realmIdName in
+            return IdName(id: realmIdName.id, name: realmIdName.name)
+        }))
+        return Movie(id: self.id, title: self.title, fullTitle: self.fullTitle, year: self.year, releaseState: self.releaseState, image: self.image, runtimeMins: self.runtimeMins, runtimeStr: self.runtimeStr, plot: self.plot, contentRating: self.contentRating, imDbRating: self.imDbRating, imDbRatingCount: self.imDbRatingCount, metacriticRating: self.metacriticRating, genres: self.genres, genreList: genreList, directors: self.directors, directorList: directorList, stars: self.stars, starList: starList)
+    }
+}
+
+class RealmKeyValue : Object {
+    @Persisted var key : String
+    @Persisted var value : String
+}
+
+class RealmIdName : Object {
+    @Persisted var id : String
+    @Persisted var name : String
 }
--- MoviesApp/ViewModel/MoviesViewModel.swift
+++ MoviesApp/ViewModel/MoviesViewModel.swift
@@ -6,10 +6,12 @@
 //
 
 import Foundation
+import RealmSwift
 
 class MoviesViewModel : ObservableObject {
     @Published var movies : [Movie] = []
     
+    @MainActor
     func getMovies() async throws -> Void {
         //TODO: api key move to variable
         if let url = URL(string: "https://my.api.mockaroo.com/movies.json?key=cb03b960") {
@@ -21,6 +23,8 @@ class MoviesViewModel : ObservableObject {
                     throw NSError(domain: "", code: 0)
                 } else {
                     self.movies = response.items
+                    self.saveMoviesCache(movies: response.items)
+                    
                 }
             } catch {
                 self.movies = getCachedMovies()
@@ -30,8 +34,32 @@ class MoviesViewModel : ObservableObject {
         }
     }
     
+    @MainActor
     func getCachedMovies() -> [Movie] {
-        return []
+        do {
+            let realm = try Realm()
+            let objects = realm.objects(RealmMovie.self)
+            let realmMovies = Array(objects)
+            return realmMovies.map { realmMovie in
+                return realmMovie.getMovie()
+            }
+        } catch {
+            return []
+        }
+    }
+    
+    @MainActor
+    func saveMoviesCache(movies: [Movie]) {
+        do {
+            let realm = try Realm()
+            try realm.write {
+                for movie in movies {
+                    realm.add(movie.getPersistedObject())
+                }
+            }
+        } catch {
+            
+        }
     }
     
 }
